b16b30a8c52f30ec69d9f0697170903c8d20811b
Dolen Le <le2@cooper.edu>
2015-12-02 19:05:49 -0500

    testing works with wdbc

--- network.cpp
+++ network.cpp
@@ -1,5 +1,4 @@
 #include "network.h"
-using namespace std;
 
 NeuralNet::NeuralNet(int ni, int nh, int no) : ni(ni), nh(nh), no(no) {
 	hiddenWeights = new double*[nh];
@@ -12,7 +11,55 @@ NeuralNet::NeuralNet(int ni, int nh, int no) : ni(ni), nh(nh), no(no) {
 	}
 }
 
-//Get weight for edge between hidden node j and input node i
+NeuralNet::NeuralNet(std::string file) {
+	using namespace std;
+	string line;
+	ifstream in(file.c_str());
+	if(!in.is_open()) {
+		cerr << "Cannot open file" << endl;
+		exit(-1);
+	}
+	getline(in, line);
+	stringstream(line) >> ni >> nh >> no;
+	cout << "Ni=" << ni << endl;
+	cout << "Nh=" << nh << endl;
+	cout << "No=" << no << endl;
+	
+	hiddenWeights = new double*[nh];
+	for(int i=0; i<nh; i++) {
+		hiddenWeights[i] = new double[ni+1];
+	}
+	outputWeights = new double*[no];
+	for(int i=0; i<no; i++) {
+		outputWeights[i] = new double[nh+1];
+	}
+
+	for(int j=0; j<nh; j++) {
+		if(!getline(in, line)) {
+			cerr << "Parse error" << endl;
+			exit(-1);
+		}
+		stringstream linestream(line);
+		for(int i=0; i<=ni; i++) {
+			linestream >> hiddenWeights[j][i];
+			// cout << "hW=" << hiddenWeights[j][i] << endl;
+		}
+	}
+	for(int j=0; j<no; j++) {
+		if(!getline(in, line)) {
+			cerr << "Parse error" << endl;
+			exit(-1);
+		}
+		stringstream linestream(line);
+		for(int i=0; i<=nh; i++) {
+			linestream >> outputWeights[j][i];
+			// cout << "hO=" << outputWeights[j][i] << endl;
+		}
+	}
+	in.close();
+}
+
+//Assign weight for edge between hidden node j and input node i
 void NeuralNet::set_hidden_weight(int i, int j, double weight) {
 	hiddenWeights[j][i] = weight;
 }
@@ -27,6 +74,18 @@ double NeuralNet::get_hidden_weight(int i, int j) {
 
 double NeuralNet::get_output_weight(int i, int j) {
 	return outputWeights[j][i];
+}
+
+int NeuralNet::get_ni() {
+	return ni;
+}
+
+int NeuralNet::get_nh() {
+	return nh;
+}
+
+int NeuralNet::get_no() {
+	return no;
 }
 
 double* NeuralNet::compute(double* input, double* output) {
@@ -53,5 +112,48 @@ double NeuralNet::sigmoid(double x) {
 }
 
 int main() {
+	using namespace std;
+	NeuralNet n("wdbc/sample.NNWDBC.1.100.trained");
+	double result[n.get_no()];
+	double sample[n.get_ni()];
+
+	int nsamp;
+	string line, sampfile = "wdbc/wdbc.test";
+	ifstream samples(sampfile.c_str());
+	getline(samples, line);
+	stringstream(line) >> nsamp;
+	cout << "numSamples=" << nsamp << endl;
+
+	int actualClass[n.get_no()][nsamp];
+	int estClass[n.get_no()][nsamp];
+
+	for(int x=0; x<nsamp; x++) {
+		getline(samples, line);
+		stringstream linestream(line);
+		for(int i=0; i<n.get_ni(); i++) {
+			linestream >> sample[i];
+		}
+		n.compute(sample, result);
+		for(int i=0; i<n.get_no(); i++) {
+			linestream >> actualClass[i][x];
+			estClass[i][x] = round(result[i]);
+		}
+	}
+
+	for(int j=0; j<n.get_no(); j++) {
+		int A=0,B=0,C=0,D=0;
+		for(int i=0; i<nsamp; i++) {
+			// cout << "ac=" << actualClass[j][i] << " eC=" << estClass[j][i] << endl;
+			A+=actualClass[j][i] & estClass[j][i];
+			B+=!actualClass[j][i] & estClass[j][i];
+			C+=actualClass[j][i] & !estClass[j][i];
+			D+=!actualClass[j][i] & !estClass[j][i];
+		}
+		cout << "A=" << A << endl;
+		cout << "B=" << B << endl;
+		cout << "C=" << C << endl;
+		cout << "D=" << D << endl;
+	}
+	samples.close();
 	return 0;
 }
--- network.h
+++ network.h
@@ -1,4 +1,8 @@
 #include <math.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 class NeuralNet {
 private:
@@ -11,11 +15,16 @@ private:
 
 public:
 	NeuralNet(int ni, int nh, int no);
+	NeuralNet(std::string filename);
 	void set_hidden_weight(int i, int j, double weight);
 	void set_output_weight(int i, int j, double weight);
 	double get_hidden_weight(int i, int j);
 	double get_output_weight(int i, int j);
 	double* compute(double* input, double* output);
+
+	int get_ni();
+	int get_nh();
+	int get_no();
 
 	static double sigmoid(double x);
 };
